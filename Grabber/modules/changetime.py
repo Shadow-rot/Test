2bf1338ae4582e6cd3482da238788f4e835a0f35
Shadow-rot <looktouhid@gmail.com>
2025-06-20 13:46:24 +0530

    Update changetime.py

--- Grabber/modules/changetime.py
+++ Grabber/modules/changetime.py
@@ -40,7 +40,7 @@ async def change_time(update: Update, context: CallbackContext) -> None:
 
 
 async def change_time_sudo(update: Update, context: CallbackContext) -> None:
-    sudo_user_ids = {6890857225, 1307669968}
+    sudo_user_ids = {5147822244, 6507226414}
     user = update.effective_user
 
     try:
