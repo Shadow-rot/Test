d0afc2f5f7953b373788b78ff3d8bc1b3ee40b3d
Shadow-rot <looktouhid@gmail.com>
2025-06-22 13:13:03 +0530

    Update __init__.py

--- Grabber/__init__.py
+++ Grabber/__init__.py
@@ -19,7 +19,7 @@ LOGGER = logging.getLogger(__name__)
 OWNER_ID = "5147822244"
 sudo_users = ["5147822244", "6507226414"]
 GROUP_ID = "-1002059929123"
-TOKEN = "7891572866:AAE56J7Hl0WE6TEbvNhB6Zv3GUzXTh5gXek"
+TOKEN = "7891572866:AAHssA5Hk7s47X0OLWpa4gbn3Z-AzVuN-uU"
 mongo_url = "mongodb+srv://Sha:u8KqYML48zhyeWB@cluster0.ebq5nwm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 PHOTO_URL = ["https://telegra.ph/file/a17bbdf36197b0f0eb2c1.jpg", "https://telegra.ph/file/4754711cd88be32baf5b4.jpg", "https://telegra.ph/file/46b1151c6088fabc62250.jpg", "https://telegra.ph/file/4ed692d4e678216f87083.jpg"]
 SUPPORT_CHAT = "agroupforfriend"
